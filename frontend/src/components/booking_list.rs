83e3af2073e0679998e28ac158fee9ec95e312b2
Philip Scott <phil@vega.xyz>
2023-01-18 23:14:00 +0000

    bongle

---
+++ frontend/src/components/booking_list.rs
@@ -0,0 +1,34 @@
+use yew::prelude::*;
+use yew_icons::{Icon, IconId};
+
+#[function_component(AddButton)]
+pub fn add_button<T>() -> Html
+where
+    T: BaseComponent,
+    T::Properties
+{
+    let modal_visible = use_state(|| false);
+    let onclick = {
+        let modal_visible = modal_visible.clone();
+        move |_| modal_visible.set(true)
+    };
+
+    html! {<>
+    <button class="button is-link" {onclick}>
+      <span class="icon">
+      <Icon icon_id={IconId::FontAwesomeRegularSquarePlus}/>
+      </span>
+      <span>{"Add User"}</span>
+    </button>
+    <T is_visible={*modal_visible} visible_changed={move |v| modal_visible.set(v)}/>
+    </>
+    }
+}
+
+#[function_component(BookingList)]
+pub fn booking_list() -> Html {
+    html! {
+    <>
+      <h1>{"Booking List"}</h1>
+    </>}
+}
